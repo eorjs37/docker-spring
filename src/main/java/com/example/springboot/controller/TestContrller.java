68193b25ef017f6786488a7ef6c3d0748cf19b90
macdaegeon <chleorjs37@gmail.com>
2023-09-25 21:45:43 +0900

    first commit

---
+++ src/main/java/com/example/springboot/controller/TestContrller.java
@@ -0,0 +1,26 @@
+package com.example.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestContrller {
+    @RequestMapping(value = "/home")
+    public String home(){
+        return  "index.html";
+    }
+
+    @ResponseBody
+    @RequestMapping(value =  "/")
+    public String helloWorld(){
+        return  "Helloss";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/homevalue")
+    public String valueTest(){
+        String value = "120";
+        return  value;
+    }
+}
